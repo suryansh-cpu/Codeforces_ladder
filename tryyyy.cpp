b6dc4dcd049c6871493d69a991fde2bf49c30f86
suryansh-cpu <lemononator23@gmail.com>
2025-10-06 13:57:01 +0530

    Auto-upload Codeforces solutions 2025-10-06 13:57:01

--- tryyyy.cpp
+++ tryyyy.cpp
@@ -1,3 +1,107 @@
+// #include <cassert>
+// #include <cctype>
+// #include <cerrno>
+// #include <cfloat>
+// #include <ciso646>
+// #include <climits>
+// #include <clocale>
+// #include <cmath>
+// #include <csetjmp>
+// #include <csignal>
+// #include <cstdarg>
+// #include <cstddef>
+// #include <cstdio>
+// #include <cstdlib>
+// #include <cstring>
+// #include <ctime>
+// #if __cplusplus >= 201103L
+// #include <ccomplex>
+// #include <cfenv>
+// #include <cinttypes>
+// #include <cstdbool>
+// #include <cstdint>
+// #include <ctgmath>
+// #include <cwchar>
+// #include <cwctype>
+// #endif
+// #include <algorithm>
+// #include <bitset>
+// #include <complex>
+// #include <deque>
+// #include <exception>
+// #include <fstream>
+// #include <functional>
+// #include <iomanip>
+// #include <ios>
+// #include <iosfwd>
+// #include <iostream>
+// #include <istream>
+// #include <iterator>
+// #include <limits>
+// #include <list>
+// #include <locale>
+// #include <map>
+// #include <memory>
+// #include <new>
+// #include <numeric>
+// #include <ostream>
+// #include <queue>
+// #include <set>
+// #include <sstream>
+// #include <stack>
+// #include <stdexcept>
+// #include <streambuf>
+// #include <string>
+// #include <typeinfo>
+// #include <utility>
+// #include <valarray>
+// #include <vector>
+// #if __cplusplus >= 201103L
+// #include <array>
+// #include <atomic>
+// #include <chrono>
+// #include <condition_variable>
+// #include <forward_list>
+// #include <future>
+// #include <initializer_list>
+// #include <mutex>
+// #include <random>
+// #include <ratio>
+// #include <regex>
+// #include <scoped_allocator>
+// #include <system_error>
+// #include <thread>
+// #include <tuple>
+// #include <typeindex>
+// #include <type_traits>
+// #include <unordered_map>
+// #include <unordered_set>
+// #endif
+// #define int long long int
+// #define endl "\n"
+// #define pb push_back
+// #define rep(i,n) for(int i=0;i<n;++i)
+// using namespace std;
+// signed main(){
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     int n;
+//     cin >> n;
+//     // vector<int>pree(n,0);
+//     int maxx = -1;
+//     for(int j= 2;j<n;j++){
+//         int i = j;
+//         int steps = 0;
+//         while(i>1){
+//             if(i%2==0)i/=2;
+//             else i = i*3 + 1;
+//             steps++;
+//         }
+//         maxx = max(steps,maxx);
+//     }
+//     cout << maxx;
+//     return 0;
+// }
 #include <cassert>
 #include <cctype>
 #include <cerrno>
@@ -85,20 +189,37 @@ using namespace std;
 signed main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n;
-    cin >> n;
-    // vector<int>pree(n,0);
-    int maxx = -1;
-    for(int j= 2;j<n;j++){
-        int i = j;
-        int steps = 0;
-        while(i>1){
-            if(i%2==0)i/=2;
-            else i = i*3 + 1;
-            steps++;
+    int t;
+    cin >> t;
+    while(t--){
+        vector<int>pre_computed(5000006,0);
+        int n;
+        cin >> n;
+        int maxx = -1;
+        int num = 0;
+        for(int i = 1;i<n;i++){
+            int count = 0;
+            int a = i;
+            while(a>1){
+                if(a<5000001 && pre_computed[a]!=0){
+                    count+=pre_computed[a];
+                    break;
+                }
+                if((a%2) == 0){
+                    a=a/2;
+                }
+                else{
+                    a = a*3 + 1;
+                }
+                count++;
+            }
+            pre_computed[i] = count;
+            if(pre_computed[i] >= maxx){
+                maxx = pre_computed[i];
+                num = i;
+            }
         }
-        maxx = max(steps,maxx);
+        cout << num << endl;
     }
-    cout << maxx;
     return 0;
 }
